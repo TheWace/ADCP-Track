0dd46c8dccde5b9be4904461aed2a749c78aaece
kigan froment <kigan.froment@hotmail.com>
2017-06-16 11:25:16 +0200

    Write into textbox CmdADCP

--- ADCP-Track/Form/CmdADCP.Designer.cs
+++ ADCP-Track/Form/CmdADCP.Designer.cs
@@ -33,13 +33,16 @@
             // 
             // textBox1
             // 
+            this.textBox1.AcceptsReturn = true;
             this.textBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
             this.textBox1.BackColor = System.Drawing.Color.White;
             this.textBox1.Location = new System.Drawing.Point(2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(779, 358);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = ">";
             this.textBox1.WordWrap = false;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
@@ -47,13 +50,15 @@
             // 
             // CmdADCP
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(783, 361);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.IsMdiContainer = true;
             this.Name = "CmdADCP";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CmdADCP";
--- ADCP-Track/Form/CmdADCP.cs
+++ ADCP-Track/Form/CmdADCP.cs
@@ -38,10 +38,10 @@ namespace ADCP_Track
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
-           // if (SerialIN.IsOpen)
-            //{
+            if (SerialIN.IsOpen)
+            {
                 if (e.KeyValue == 72) { SerialIN.Write("==="); };
-           // } 
+            } 
         }
 
         char[] tempC = new char[1];
@@ -50,12 +50,30 @@ namespace ADCP_Track
         {
             tempC[0] = e.KeyChar;
             byte[] byte0 = enc.GetBytes(tempC);
-            //if (SerialIN.IsOpen) // && tempC[0] != (char)35)
-           // {
-                SerialIN.Write(byte0, 0, byte0.Length);
-          //  }
+            if (SerialIN.IsOpen) 
+            {
 
+                SerialIN.Write(byte0, 0, byte0.Length);
+                if (tempC[0] == (char)8 && textBox1.Text.ToString() != ">")//Backspace
+                {
+
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+
+                }
+                else if (tempC[0] == (char)13)//Enter
+                {
+                textBox1.Text += Environment.NewLine +">";
+                }
+                else
+                {
+                    textBox1.Text += e.KeyChar.ToString().ToUpper();
+                }
+                textBox1.Focus();
+                textBox1.SelectionStart = textBox1.Text.Length;
+                
+            }
             e.Handled = true;
+           
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
